b9da2337d9daa9750d42a9a7c9da4713fb149e70
Jaswinder Kaur <jkaur79@nait.ca>
2021-02-27 16:22:10 -0700

    next

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path')
+// creating an instance of express
+const app = express();
+const PORT =  process.env.PORT || 5000 
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.join(__dirname, "../client"),{
+       dotfiles: "ignore",
+        extensions: ["html","htm"]
+    })
+  );
+
+  app.post('/login',(req, res)=>{
+      console.log('here it is');
+    console.log(req.body);
+    res.send("trying to login");
+    })
+
+
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "../client/404.html"));
+  }); 
+
+
+app.listen(PORT, ()=>{
+    console.log(`server listening at http://localhost:${PORT}`)
+})
