6b3bdbda895cd6e63d65868bbc0e25724f9c1283
Jaswinder Kaur <jkaur79@nait.ca>
2021-02-27 23:44:17 -0700

    login functionality

--- server/index.js
+++ server/index.js
@@ -2,26 +2,64 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const session = require('express-session');
 // creating an instance of express
 const app = express();
+const loginService = require('./services/loginService');
 const PORT =  process.env.PORT || 5000 
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'./views'))
 app.use(express.static(path.join(__dirname, "../client"),{
        dotfiles: "ignore",
         extensions: ["html","htm"]
     })
   );
 
+  app.get('/dashboard',(req, res)=>{
+      if(req.session.isValid){
+        res.render('dashboard')
+      }else{
+        res.render('/login')
+      }
+     
+  })
+
+  app.get('/login',(req,res)=>{
+      res.render('login',{emailWarning:"", passwordWarning:"",email:"",password:""})
+  });
+
   app.post('/login',(req, res)=>{
-      console.log('here it is');
     console.log(req.body);
    // res.send("trying to login");
-    res.sendFile(path.join(__dirname, "../client/dashboard.html"));
+   const credentials ={
+       email:req.body.email,
+       password:req.body.password
+   }
+   const isValidUser = loginService.authenticate(credentials)
+   if(isValidUser.user !== null){
+    if(!req.session.isValid){
+         req.session.isValid = true;
+    }
+    res.redirect('dashboard');
+  }
+
+  if(isValidUser.user === null){
+     res.render('login',{
+       emailWarning:isValidUser.emailWarning,
+       passwordWarning:isValidUser.passwordWarning,
+       email:req.body.email,
+       password:req.body.password
+     })
+  }
+    //res.sendFile(path.join(__dirname, "../client/dashboard.html"));
+   // res.end();
     })
 
     app.post('/register',(req, res)=>{
@@ -30,7 +68,9 @@ app.use(express.static(path.join(__dirname, "../client"),{
      // res.send("trying to register");
       })
 
-
+app.get('/api/users',(req,res)=>{
+    res.send('users api starting');
+})
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "../client/404.html"));
--- server/services/loginService.js
+++ server/services/loginService.js
@@ -1,9 +1,39 @@
 const fileService = require('./fileService')
 
-const authenticate = (credentials)=>{
-  const {email, password} = {...credentials}
+exports.authenticate = (credential)=>{
+    console.log(credential)
+  const {email, password} = {...credential}
   const users = fileService.getFileContents('../data/users.json');
   console.log(email, password)
+  const authUser = users.reduce((authObj,user)=>{
+     if(user.email === email){
+         authObj.validEmail = true;
+     }
+     if(user.password === password){
+        authObj.validPassword = true;
+    }
+    if(authObj.validEmail === true && authObj.validPassword === true){
+        authObj.user = user;
+    }
+    return authObj;
+  },{validEmail:false,validPassword:false, user:null})
+
+  console.log(authUser)
+  const auth0 = authUser.user ? {user:authUser.user}:formatErrors(authUser);
+  return auth0;
 }
 
-authenticate({email:"jass",password:"1234"})
+const formatErrors = function(authObj){
+let emailWarning = '';
+let passwordWarning = '';
+if(authObj.validEmail === false){
+    emailWarning ="Email doesn't exist";
+}
+
+if(authObj.validPassword === false){
+    passwordWarning ="Password doesn't exist";
+}
+
+return {user:null,emailWarning, passwordWarning}
+
+}
